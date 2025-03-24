7068f790a2278b632e357775e7bdbd3245a7ce4e
Zeinab Ganjei <zganjei@users.noreply.github.com>
2025-03-24 10:08:13 +0000

    hydration error resolved. local host not match resolved

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-// module.exports = {
-//   experimental: {
-//     serverActions: {
-//       allowedOrigins: [
-//         'localhost:3000', // localhost
-//         'https://sturdy-system-jjjqjjqqxjvfjg4-3000.app.github.dev/', // Codespaces
-//       ],
-//     },
-//   },
-// };
+
+module.exports = {
+    experimental: {
+      serverActions: {
+        allowedOrigins: [
+          'localhost:3000', // localhost
+          'foobar-3000.app.github.dev', // Codespaces
+        ],
+      },
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.clerk.com',
+        },
+      ],
+    },
+  };
+  

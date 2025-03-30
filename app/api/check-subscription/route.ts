6c3b6fd93b981034e6cb507cd09181dbda652334
Zeinab Ganjei <zganjei@users.noreply.github.com>
2025-03-30 12:08:01 +0000

    check-subscription added

---
+++ app/api/check-subscription/route.ts
@@ -0,0 +1,26 @@
+import {prisma} from "@/lib/prisma"
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function GET(request:NextRequest){
+    try{
+        const {searchParams} = new URL(request.url)
+        const userId = searchParams.get("userId")
+
+        if(!userId){
+            return NextResponse.json({error:"Missing userId."}, {status:400})
+        }
+
+        const profile = await prisma?.profile.findUnique({
+            where: {userId},
+            select: {subscriptionActive: true},
+        });
+        
+        return NextResponse.json({
+            subscriptionActive: profile?.subscriptionActive,
+        });
+
+    }catch(error:any){
+        return NextResponse.json({error: "Internal Error.", }, {status:500});
+    }
+}
--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-up(.*)",
   "/subscribe(.*)",
   "/api/webhook(.*)",
+  "/api/check-subscription(.*)",
+
 ]);
 
 const isSignupRoute = createRouteMatcher(["/sign-up(.*)",]);
@@ -21,13 +23,17 @@ export default clerkMiddleware(async (auth, req) => {
   const userAuth = await auth();
   const { userId } = userAuth;
 
-  // specific to codespaces
+  // // specific to codespaces
   const isLocalhost = req.nextUrl.origin.includes("localhost");
   const origin = isLocalhost
     ? process.env.NEXT_PUBLIC_API_URL // Use the Codespaces URL
     : req.nextUrl.origin; // Otherwise, use the request's origin
 
   console.log("Middleware info: ", userId, pathname, origin);
+
+  if(pathname === "/api/check-subscription"){
+    return NextResponse.next();
+  }
 
   if (!isPublicRoute(req) && !userId) {
     return NextResponse.redirect(new URL("/sign-up", origin));
@@ -38,7 +44,17 @@ export default clerkMiddleware(async (auth, req) => {
   }
 
   if (isMealPlanRoute(req) && userId){
-    
+
+    try{
+      const response = await fetch(`${origin}/api/check-subscription?userId=${userId}`)
+      const data = await response.json()
+      if(!data.subscriptionActive){
+        return NextResponse.redirect(new URL("/subscribe",origin));
+      }
+    }catch(error: any){
+      return NextResponse.redirect(new URL("/subscribe",origin));
+    }
+
   }
 
   return NextResponse.next();
